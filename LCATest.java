1f3cd6f182d0ff7d87d9d094352795a524129f7b
Jane616 <55512796+Jane616@users.noreply.github.com>
2019-09-28 21:39:13 +0100

    add findpath tests - invalid input & no iteration

--- LCATest.java
+++ LCATest.java
@@ -36,11 +36,28 @@ public class LCATest
     	
     }
     
+    //test for findpath method with invalid input
+    @Test
+    public void testfindpathInvalid() {
+    	LCASolution testSolution = new LCASolution();
+    	assertFalse("findpathInvalid test failed", testSolution.findpath(null, 2, testSolution.path1));
+    }
     
-
-
+    //test for findpath method with no iteration
+    @Test
+    public void testfindpathNoIteration() {
+    	Node testRoot = new Node(3);
+    	testRoot.left = new Node(1);
+    	testRoot.right = new Node(2);
+    	ArrayList<Integer> testpath = new ArrayList<Integer>();
+    	testpath.add(3);
+    	
+    	
+    	LCASolution testSolution = new LCASolution();
+    	assertTrue("findpathNoIteration test failed - return value", testSolution.findpath(testRoot, 3, testSolution.path1));
+    	assertEquals("findpathNoIteration test failed - recorded path", testpath, testSolution.path1);
+    }
     
-   
 
     
 }
