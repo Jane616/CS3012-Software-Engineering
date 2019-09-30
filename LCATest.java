5311d3a5bc9f1b461854d2d8a2ecac976163694e
Jane616 <55512796+Jane616@users.noreply.github.com>
2019-09-30 23:05:03 +0100

    add findpath test for depth 3 binary tree

--- LCATest.java
+++ LCATest.java
@@ -58,6 +58,57 @@ public class LCATest
     	assertEquals("findpathNoIteration test failed - recorded path", testpath, testSolution.path1);
     }
     
+    //test for findpath method with a depth 3 binary tree
+    @Test
+    public void testfindpathDepth3Binary() {
+    	Node testRoot = new Node(1);
+    	testRoot.left = new Node(2);
+    	testRoot.right = new Node(3);
+    	testRoot.left.left = new Node(4);
+    	testRoot.left.right = new Node(5);
+    	testRoot.right.left = new Node(6);
+    	testRoot.right.right = new Node(7);
+    	
+    	ArrayList<Integer> testpath2 = new ArrayList<Integer>();
+    	testpath2.add(1);
+    	testpath2.add(2);
+    	
+    	ArrayList<Integer> testpath3 = new ArrayList<Integer>();
+    	testpath3.add(1);
+    	testpath3.add(3);
+    	
+    	ArrayList<Integer> testpath5 = new ArrayList<Integer>();
+    	testpath5.add(1);
+    	testpath5.add(2);
+    	testpath5.add(5);
+    	
+    	ArrayList<Integer> testpath6 = new ArrayList<Integer>();
+    	testpath6.add(1);
+    	testpath6.add(3);
+    	testpath6.add(6);
+    	
+    	ArrayList<Integer> testpath8 = new ArrayList<Integer>();
+    	
+    	LCASolution testSolution = new LCASolution();
+    	assertTrue("findpathDepth3Binary test failed - return value - 2", testSolution.findpath(testRoot, 2, testSolution.path1));
+    	assertEquals("findpathDepth3Binary test failed - recorded path - 2", testpath2, testSolution.path1);
+    	
+    	testSolution.path1.clear();
+    	assertTrue("findpathDepth3Binary test failed - return value - 3", testSolution.findpath(testRoot, 3, testSolution.path1));
+    	assertEquals("findpathDepth3Binary test failed - recorded path - 3", testpath3, testSolution.path1);
+    	
+    	testSolution.path1.clear();
+    	assertTrue("findpathDepth3Binary test failed - return value - 5", testSolution.findpath(testRoot, 5, testSolution.path1));
+    	assertEquals("findpathDepth3Binary test failed - recorded path - 5", testpath5, testSolution.path1);
+    	
+    	testSolution.path1.clear();
+    	assertTrue("findpathDepth3Binary test failed - return value - 6", testSolution.findpath(testRoot, 6, testSolution.path1));
+    	assertEquals("findpathDepth3Binary test failed - recorded path - 6", testpath6, testSolution.path1);
+    	
+    	testSolution.path1.clear();
+    	assertFalse("findpathDepth3Binary test failed - return value - 8", testSolution.findpath(testRoot, 8, testSolution.path1));
+    	assertEquals("findpathDepth3Binary test failed - recorded path - 8", testpath8, testSolution.path1);
+    }
 
     
 }
