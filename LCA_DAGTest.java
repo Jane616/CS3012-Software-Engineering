f9ed5afb12bbd69c0ef6840b581f5f62c9d9a892
Jane616 <55512796+Jane616@users.noreply.github.com>
2019-10-08 21:43:18 +0100

    add tests for DAG special cases

---
+++ LCA_DAGTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+
+//-------------------------------------------------------------------------
+/**
+ *  Test class for LCA_DAG.java
+ * 	special test cases for DAG
+ *
+ */
+@RunWith(JUnit4.class)
+public class LCA_DAGTest
+{
+
+
+    
+}
