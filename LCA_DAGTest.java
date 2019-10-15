4e0e049c885bd0bba6e6021d65f2f63c39eeaefa
Jane616 <55512796+Jane616@users.noreply.github.com>
2019-10-15 23:26:05 +0100

    add constructor test

--- LCA_DAGTest.java
+++ LCA_DAGTest.java
@@ -15,7 +15,25 @@ import java.util.ArrayList;
 @RunWith(JUnit4.class)
 public class LCA_DAGTest
 {
-
-
+	//test for Node class constructor
+    @Test
+    public void testNodeConstructor()
+    {
+      Node test = new Node(2);
+      assertEquals("Node class constructor testing failed. - test.data", 2, test.data);
+      assertNull("Node class constructor testing failed. - test.left", test.left);
+      assertNull("Node class constructor testing failed. - test.right", test.right);
+    }
     
-}
+    //test for LCASolution class constructor
+    @Test
+    public void testLCASolutionConstructor()
+    {
+    	LCASolution testSolution = new LCASolution();
+    	assertEquals("LCASolution class constructor testing failed - testSolution.lca", -1, testSolution.lca);
+    	assertNotNull("LCASolution class constructor testing failed - testSolution.path1", testSolution.path1);
+    	assertNotNull("LCASolution class constructor testing failed - testSolution.path2", testSolution.path2);
+    	assertNotNull("LCASolution class constructor testing failed - testSolution.data", testSolution.data);
+    	assertNotNull("LCASolution class constructor testing failed - testSolution.curpath", testSolution.curpath);
+    	
+    }
